298989a9fab94342c56c8721e55c35114eb63408
Artem Karpovich <karpartem94@gmail.com>
2016-07-14 18:31:13 +0300

    Implemente page for display links by tag

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { routerReducer } from 'react-router-redux';
 import user from './user';
 import link from './link';
 import links from './links';
+import linksByTag from './linksByTag';
 
 
 export default combineReducers({
   routing: routerReducer,
   user,
   link,
-  links
+  links,
+  linksByTag,
 });
